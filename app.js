67bc4ba71a88cb25c0507a89a8f56921b9edcd3b
Erfan-Pahlevan <erfan.pahlevan82@gmail.com>
2026-06-21 19:50:31 +0330

    Add user roles constant, enhance user validation, and implement security middleware

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const express = require("express");
-const {
-  checkAuthHeader,
-  auth,
-} = require("./middlewares/users/users.middleware");
+const { auth } = require("./middlewares/users/users.middleware");
 const userRoutes = require("./routes/users.routes");
 const postRoutes = require("./routes/posts.routes");
 const loggerMiddleware = require("./middlewares/logger.middleware");
 const errorMiddleware = require("./middlewares/error.middleware");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/shop"),
-      console.log("connected");
+    (await mongoose.connect("mongodb://127.0.0.1:27017/shop"),
+      console.log("connected"));
   } catch (err) {
     console.error(err);
   }
@@ -21,14 +21,18 @@ async function connectDB() {
 
 connectDB();
 
-// Morgan middleware
-// const morgan = require("morgan");
-// app.use(morgan("tiny"));
 const app = express();
 
 // middleware for parsing the body
 app.use(express.json());
 
+app.use(cors({ origin: "https://frontend.com" }));
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+app.use(helmet());
 app.use(loggerMiddleware);
 
 app.use("/uploads", express.static("uploads"));
