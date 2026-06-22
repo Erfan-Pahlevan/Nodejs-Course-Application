6d0d37e8e8dde383a3e25e1e6e8500901a41d09f
Erfan-Pahlevan <erfan.pahlevan82@gmail.com>
2026-06-22 19:43:25 +0330

    fix: read statusCode from error in error middleware

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,6 +1,7 @@
 const errorMiddleware = (err, req, res, next) => {
-  console.log(err);
-  return res.status(500).json({
+  console.error(err);
+  const statusCode = err.statusCode || 500;
+  return res.status(statusCode).json({
     message: "Something went wrong",
     error: err.message,
   });
